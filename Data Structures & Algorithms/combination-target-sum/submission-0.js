272bc33156a9db34263de5ab9d2259fcb2014996
Patrick Crown-Milliss <patrickcrownmilliss@gmail.com>
2026-05-10 19:06:23 +0100

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(nums, target) {
+        let res = []
+
+        let dfs = (arr, index) => {
+            let val = arr.reduce((el, total) => total + el, 0)
+            // console.log("considering:", arr)
+            // console.log("val:", val)
+            if (val === target) {
+                res.push([...arr])
+                return;
+            }
+            if (val > target) return;
+            if (index >= nums.length) return;
+
+            for (let i = 0; i <= target - val; i += nums[index]) {
+                arr.push(nums[index])
+                dfs(arr, index + 1)
+            }
+            for (let i = 0; i <= target - val; i += nums[index]) {
+                arr.pop()
+            }
+            dfs(arr, index + 1)
+        }
+        dfs([], 0)
+        return res
+    }
+}
