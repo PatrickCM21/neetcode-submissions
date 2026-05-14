abdff575596832ae73c0a2785eb573c336a52d45
Patrick Crown-Milliss <patrickcrownmilliss@gmail.com>
2026-05-14 14:31:46 +0100

    Add: coin-change - submission-6

---
+++ Data Structures & Algorithms/coin-change/submission-6.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} coins
+     * @param {number} amount
+     * @return {number}
+     */
+    coinChange(coins, amount) {
+        let dp = new Array(amount + 1).fill(Infinity)
+        dp[0] = 0
+        for (let i = 1; i <= amount; i++) {
+            for (let coin of coins) {
+                if (i - coin < 0) continue
+                dp[i] = Math.min(dp[i], 1 + dp[i - coin])
+            }
+        }
+        return dp[amount] !== Infinity ? dp[amount] : -1
+    }
+}
