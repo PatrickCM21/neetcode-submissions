261a17a02457922bbe5436c7397355918ae09b5c
Patrick Crown-Milliss <patrickcrownmilliss@gmail.com>
2026-05-15 11:11:24 +0100

    Add: palindromic-substrings - submission-3

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-3.js
@@ -0,0 +1,61 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    countSubstrings(s) {
+        let total = 0;
+
+        let memo = new Map()
+
+        let palindromeCheck = (str) => {
+            if (memo.has(str)) return memo.get(str)
+            if (str[0] === str[str.length - 1]) {
+                //console.log(str, 'is a plaindrome')
+                memo.set(str, true)
+                return true
+            } else {
+                memo.set(str, false) 
+                return false
+            }
+        }
+
+        let helper = (i) => {
+            let subStr = s[i]
+            let offset = 1
+            while (palindromeCheck(subStr)) {
+                //console.log(subStr, "is a palindrome")
+                total++
+                if (i - offset >= 0 && i + offset < s.length) {
+                    subStr = subStr.split('')
+                    subStr.unshift(s[i - offset])
+                    subStr.push(s[i + offset])
+                    subStr = subStr.join('')
+                    offset++;
+                } else {
+                    break;
+                }
+            }
+            if (i + 1 === s.length) return;
+            // right check
+            offset = 1;
+            subStr = s[i].concat(s[i + 1])
+            while (palindromeCheck(subStr)) {
+                //console.log(subStr, "is a palindrome")
+                total++
+                if (i - offset >= 0 && i + 1 + offset < s.length) {
+                    subStr = subStr.split('')
+                    subStr.unshift(s[i - offset])
+                    subStr.push(s[i + 1 + offset])
+                    subStr = subStr.join('')
+                    offset++;
+                } else {
+                    break;
+                }
+            }
+            helper(i + 1)
+        }
+        helper(0)
+        return total
+    }
+}
